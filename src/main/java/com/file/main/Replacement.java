2e910794d5d8616a67d32b567e59ca429b419169
aa <jinhailong@camelotchina.com>
2020-11-12 10:15:31 +0800

    迁移代码目录1

--- src/main/java/com/file/main/Replacement.java
+++ src/main/java/com/file/main/Replacement.java
@@ -5,48 +5,48 @@ import java.util.List;
 
 class Replacement {
 
-	private List<ReplaceObj> includeList = new ArrayList<>();// 替换信息
+    private final List<ReplaceObj> includeList = new ArrayList<>();// 替换信息
 
-	void addInclude(String oldStr, String newStr) {
-		this.includeList.add(new ReplaceObj(oldStr, newStr));
-	}
+    void addInclude(String oldStr, String newStr) {
+        this.includeList.add(new ReplaceObj(oldStr, newStr));
+    }
 
-	List<ReplaceObj> getIncludeList() {
-		return includeList;
-	}
+    List<ReplaceObj> getIncludeList() {
+        return includeList;
+    }
 
-	class ReplaceObj {
+    static class ReplaceObj {
 
-		ReplaceObj(String oldStr, String newStr) {
-			this.oldStr = oldStr;
-			this.newStr = newStr;
-		}
+        ReplaceObj(String oldStr, String newStr) {
+            this.oldStr = oldStr;
+            this.newStr = newStr;
+        }
 
-		private String oldStr;
-		private String newStr;
+        private final String oldStr;
+        private final String newStr;
 
-		String getOldStr() {
-			return oldStr;
-		}
+        String getOldStr() {
+            return oldStr;
+        }
 
-		String getNewStr() {
-			return newStr;
-		}
+        String getNewStr() {
+            return newStr;
+        }
 
-	}
+    }
 
-	/**
-	 * 替换类型
-	 *
-	 * @author learrings
-	 * @createDate 2017年12月29日
-	 */
-	enum ReplaceType {
-		/* 只更新文件内容 */
-		CONTENT,
-		/* 只更新文件名 */
-		FILE,
-		/* 全部 */
-		All
-	}
+    /**
+     * 替换类型
+     *
+     * @author learrings
+     * @createDate 2017年12月29日
+     */
+    enum ReplaceType {
+        /* 只更新文件内容 */
+        CONTENT,
+        /* 只更新文件名 */
+        FILE,
+        /* 全部 */
+        All
+    }
 }
